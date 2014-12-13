c26bc426d980164ada851c456fb94b511f9b2b1d
ogilbert7340 <ogilbert7340@sohamcollege.org.uk>
2014-12-13 12:52:40 +0000

    Sorting out If/Else statements

--- guessWho/ghfunctions.py
+++ guessWho/ghfunctions.py
@@ -45,16 +45,24 @@ def getCharProfile():
      hatFine = False
      while hatFine != True:
           hat = input("Are you wearing a hat? ").lower()
-          if hat == "y" or "yes":
+          if hat == "y":
                hatFine = True
-          else:
+          elif hat == "yes":
+               hatFine = True
+          elif hat == "":
                hatFine = False
           
      #Eye Colour
      eyeColourFine = False
      while eyeColourFine != True:
           eyeColour = input("What colour are your eyes? ").lower()
-          if eyeColour == "blue" or "brown" or "green" or "hazel":
+          if eyeColour == "blue":
+               eyeColourFine = True
+          elif eyeColour == "brown":
+               eyeColourFine = True
+          elif eyeColour == "green":
+               eyeColourFine = True
+          elif eyeColour == "hazel":
                eyeColourFine = True
           else:
                eyeColourFine = False
@@ -63,9 +71,9 @@ def getCharProfile():
      genderFine = False
      while genderFine != True:
           gender = input("Are you a boy or a girl? ").lower()
-          if gender == "boy" or "male" or "b" or "boy":
+          if gender == "boy":
                genderFine = True
-          elif gender == "girl" or "female" or "g" or "f":
+          elif gender == "girl":
                genderFine = True
           else:
                genderFine = False
@@ -74,16 +82,22 @@ def getCharProfile():
      glassesFine = False
      while glassesFine != True:
           glasses = input("Are you wearing any glasses in the photo? ").lower()
-          if glasses == "y" or "yes":
+          if glasses == "y":
                glassesFine = True
+          elif glasses == "yes":
+               glassesFine = True
+          elif glasses == "":
+               glassesFine = False
 
      #Facial Hair
      facialHairFine = False
      while facialHairFine != True:
           facialHair = input("Do you even facial hair? ").lower()
-          if facialHair == "y" or "yes":
+          if facialHair == "y":
                facialHairFine = True
-          else:
+          elif facialHair == "yes":
+               facialHairFine = True
+          elif facialHair == "":
                facialHairFine = False
 
      return name,hairColour,eyeColour,hat,gender,glasses,facialHair
