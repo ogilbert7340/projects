29e2a7117d6dee37a19211e956d36faeb5cb7068
ogilbert7340 <ogilbert7340@sohamcollege.org.uk>
2014-12-13 12:43:11 +0000

    Adding Comments

--- minecraft/Gravel_Rain2.py
+++ minecraft/Gravel_Rain2.py
@@ -1,8 +1,8 @@
 import time, random, mcpi.minecraft as minecraft
 mc = minecraft.Minecraft.create()
 
-while True:
-    pos = mc.player.getPos()
-    mc.setBlock(pos.x+random.randint(-10,+10),50,pos.z+random.randint(-10,+10),13)
-    time.sleep(0.2)
+while True: #creates potentially infinite loop
+    pos = mc.player.getPos() #gets player position
+    mc.setBlock(pos.x+random.randint(-10,+10),50,pos.z+random.randint(-10,+10),13) #randomly places gravel blocks in relation to player location
+    time.sleep(0.2) #waits 0.2 seconds
 	
