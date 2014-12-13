6a6dcd3225ea6ef2869f47ca99e2a40005f7e7f5
ogilbert7340 <ogilbert7340@sohamcollege.org.uk>
2014-12-13 12:41:42 +0000

    Adding Comments

--- camera/Validation example.py
+++ camera/Validation example.py
@@ -1,15 +1,17 @@
 userName = ""
 while userName=="":
-    userName = input("What is your name?")
+    userName = input("What is your name?") #asks user for there name
 
-print("Good Morning {0}".format(userName))
+print("Good Morning {0}".format(userName)) #prints good morning <user>
 
+#setting variables
 password = "cheese"
 question = ""
 
+#while loop testing for correct password
 while password != question:
-    question = input("What is the Password?")
+    question = input("What is the Password?") #repeats the question until correct
     if password == "cheese":
-        print("Correct. Welcome {0}.".format(userName))
+        print("Correct. Welcome {0}.".format(userName)) #when correct, stops repeating the question and say 'Correct. Welcome <user>'
         
     
