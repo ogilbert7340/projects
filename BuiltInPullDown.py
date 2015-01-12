4c5529d4cfbb89fd61a7e8ac380eff13e4a055e8
ogilbert7340 <ogilbert7340@sohamcollege.org.uk>
2015-01-12 10:15:56 +0000

    Pull Up/ Pull Down

--- BuiltInPullDown.py
+++ BuiltInPullDown.py
@@ -3,9 +3,9 @@ import RPi.GPIO as GPIO, time
 pin = 4
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(pin, GPIO.IN, GPIO.PUD_DOWN)
 
 while True:
 	pin_value = GPIO.input(pin)
-	print ("HIGH" if pin_value else "LOW")
+	print ("Connected" if pin_value else "Disconnected")
 	time.sleep(0.5)
