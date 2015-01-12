f3e3663528ece7389f4618e3b2a3c2ca46e5aeb4
ogilbert7340 <ogilbert7340@sohamcollege.org.uk>
2015-01-12 09:57:34 +0000

    Built in Pull Up

---
+++ BuiltInPullUp.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO, time
+
+pin = 4
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.IN, GPIO.PUD_UP)
+
+while True:
+	pin_value = GPIO.input(pin)
+	print ("HIGH") if pin_value else ("LOW")
+	time.sleep(0.5)
